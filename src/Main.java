7e0e8fddafe5c27e64b54cab2c3f392967e722fd
КириллДоминов <spotik321@gmail.com>
2024-09-01 18:23:46 +0500

    Done2!

--- src/Main.java
+++ src/Main.java
@@ -11,39 +11,37 @@ public class Main {
         System.out.println("Задача № 2");
         byte clientDeviceOS = 1;
         int clientDeviceYear = 2013;
-        if ((clientDeviceOS==0)&&(clientDeviceYear<2015)) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
-        else if ((clientDeviceOS==1)&&(clientDeviceYear<2015)) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");}
-        else if (clientDeviceYear>=2015) {
-            System.out.println("Установите приложение");}
+        if ((clientDeviceOS == 0) && (clientDeviceYear < 2015)) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if ((clientDeviceOS == 1) && (clientDeviceYear < 2015)) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear >= 2015) {
+            System.out.println("Установите приложение");
+        }
         System.out.println();
         System.out.println("Задача № 3");
-        int year = 2016;
-        if (year % 400==0) {
+        int year = 2024;
+        if ((year % 400 == 0) || (year % 4 == 0)) {
             System.out.println(year + " год является високосным");
         } else if (year % 100 == 0) {
             System.out.println(year + " год не является високосным");
-        }
-        else if (year % 4 ==0) {
-            System.out.println(year + " год является високосным");}
-        else System.out.println(year+ " год не является високосным");
+        } else System.out.println(year + " год не является високосным");
         System.out.println();
         System.out.println("Задача № 4");
         int deliveryDistance = 95;
         int deliveryDay = 1;
-        if (deliveryDistance>100) {
-            System.out.println("Нет доставки");}
-        else if (deliveryDistance>20) {
-            deliveryDay++;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней " + deliveryDay);
+        } else if ((deliveryDistance > 20) && (deliveryDistance < 60)) {
+            System.out.println("Потребуется дней " + (deliveryDay + 1));
+        } else if ((deliveryDistance > 60) && (deliveryDistance < 100)) {
+            System.out.println("Потребуется дней " + (deliveryDay + 2));
+        } else {
+            System.out.println("Доставки нет");
         }
-        else if (deliveryDistance>60){
-            deliveryDay++;
-        }
-        System.out.println("доставка занимает "+deliveryDay + " суток");
         System.out.println();
         System.out.println("Задача № 5");
-        int mounthNumber = 11;
+        int mounthNumber = 12;
         switch (mounthNumber) {
             case 12:
             case 1:
